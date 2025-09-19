10d30449bdba41e5b1f9411c8de0894d87ad7468
Francisco <lysar1x950@gmail.com>
2025-09-19 10:52:54 -0600

    feat(bindings): Implement service injection and property binding

---
+++ src/app/clients-details.component/clients-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-details',
+  imports: [],
+  templateUrl: './clients-details.html',
+  styleUrl: './clients-details.scss'
+})
+export class ClientsDetails {
+
+}
--- src/app/dashboard.component/dashboard.component.ts
+++ src/app/dashboard.component/dashboard.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientDataService } from '../client-data-service';
 import { Client } from '../client.interface';
+import { ClientListComponent } from '../client-list.component/client-list.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [ClientListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
   clients: Client[] = [];
 
-  constructor(private clientDataService: ClientDataService) {}
+  constructor(private clientDataService: ClientDataService) {} //es la inyección de dependencias. Le dice a Angular: "Dame una instancia de ClientDataService".
 
   ngOnInit(): void {
     this.clients = this.clientDataService.getClients();
